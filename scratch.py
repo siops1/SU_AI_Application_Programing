bf0fa16589f65cbe0473169ce53b2be5ddf00698
HaNeul <84830345+siops1@users.noreply.github.com>
2023-04-17 00:02:45 +0900

    1차 수정본

--- scratch.py
+++ scratch.py
@@ -6,7 +6,13 @@ class kiosk:
     __menu_list = {} #메뉴, 개수
     __menu_sum_list = {} #메뉴, 총합
 
-    def menu_print(self):
+    def menu_dict(self, menu, cost): #딕셔너리에 메뉴 추가
+        self.__dict[menu] = cost
+
+    def menu_sel(self): #주문할 메뉴 입력
+        self.__sel = int(input('메뉴를 선택해주세요(1~%d) : ' % len(self.__dict)))
+
+    def menu_print(self): #메뉴 출력
         print('-'*25)
         print("\t SU SUMMER CAFE")
         print('-' * 25)
@@ -15,15 +21,8 @@ class kiosk:
             print("%d. %s\t\t%s원"%(inx, menu, cost))
             inx += 1
         print('-' * 25)
-        if len(self.__menu_list) != 0:
-            for i in self.__menu_list:
-                print("%s"%i)
-            print("총합 : %d원"%self.sum)
 
-    def menu_dict(self, menu, cost):
-        self.__dict[menu] = cost
-
-    def menu_sum(self):
+    def menu_order(self): #주문 처리
         while True:
             menu = list(self.__dict.keys())[self.__sel-1]
             cost = list(self.__dict.values())[self.__sel-1]
@@ -41,10 +40,7 @@ class kiosk:
 
             return plus_sel.lower()
 
-    def menu_sel(self):
-        self.__sel = int(input('메뉴를 선택해주세요(1~%d) : '%len(self.__dict)))
-
-    def menu_list_up(self):
+    def menu_list_up(self): #최종 주문 내역 출력
         sum = 0
         print('-'*25)
         for menu, count in self.__menu_list.items():
@@ -52,19 +48,21 @@ class kiosk:
             sum = sum + self.__dict[menu] * count
         print('-' * 25)
         print("\t\t\t    %d"%sum)
+
 kiosk_1 = kiosk()
+#추후 파일 입출력 방식으로 변환
 kiosk_1.menu_dict('에스프레소', 4000)
 kiosk_1.menu_dict('마끼아또', 4000)
 kiosk_1.menu_dict('밀크파르페', 6500)
 kiosk_1.menu_dict('아이스라떼', 4400)
-
-return_sel = 'y' \
-
-kiosk_1.menu_print()
 while True:
-    if  return_sel == 'y':
-        kiosk_1.menu_sel()
-        return_sel = kiosk_1.menu_sum()
-    else:
-        kiosk_1.menu_list_up()
-        break
+    return_sel = 'y'
+    kiosk_1.menu_print()
+    while True:
+        if  return_sel == 'y':
+            kiosk_1.menu_sel()
+            return_sel = kiosk_1.menu_order()
+        else:
+            kiosk_1.menu_list_up()
+            break
+    time.sleep(5)
